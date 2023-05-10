881b224ada65b1161cbb7716b1c8fe51fec33f2d
eggmaker <1402461065@qq.com>
2023-05-10 16:39:33 +0800

    race

---
+++ race4/race.py
@@ -0,0 +1,156 @@
+import os
+import torch
+import torchvision
+from torch import nn
+from d2l import torch as d2l
+
+
+#demo为True时，下载小样本数据
+d2l.DATA_HUB['dog'] = (d2l.DATA_URL + 'kaggle_dog.zip',
+                       '0cb91d09b814ecdc07b50f31f8dcad3e81d6a86d')
+
+demo = True
+
+if demo:
+    data_dir = d2l.download_extract('dog_tiny')
+else:
+    data_dir = os.path.join('..', 'data', 'dog-breed-identification')
+
+
+#整理数据集
+def reorg_dog_data(data_dir, valid_ratio):
+    labels = d2l.read_csv_labels(os.path.join(data_dir, 'labels.csv'))
+    d2l.reorg_train_valid(data_dir, labels, valid_ratio)
+    d2l.reorg_test(data_dir)
+
+batch_size = 32 if demo else 128
+valid_ratio = 0.1
+reorg_dog_data(data_dir, valid_ratio)
+
+
+#图像增广
+transform_train = torchvision.transforms.Compose([
+    torchvision.transforms.RandomResizedCrop(224, scale=(0.08, 1.0),
+                                             ratio=(3.0 / 4.0, 4.0 / 3.0)),
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.ColorJitter(brightness=0.4, contrast=0.4,
+                                       saturation=0.4),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                     [0.229, 0.224, 0.225])])
+
+transform_test = torchvision.transforms.Compose([
+    torchvision.transforms.Resize(256),
+    torchvision.transforms.CenterCrop(224),
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize([0.485, 0.456, 0.406],
+                                     [0.229, 0.224, 0.225])])
+
+
+#数据集训练
+train_ds, train_valid_ds = [torchvision.datasets.ImageFolder(
+    os.path.join(data_dir, 'train_valid_test', folder),
+    transform=transform_train) for folder in ['train', 'train_valid']]
+
+valid_ds, test_ds = [torchvision.datasets.ImageFolder(
+    os.path.join(data_dir, 'train_valid_test', folder),
+    transform=transform_test) for folder in ['valid', 'test']]
+
+train_iter, train_valid_iter = [torch.utils.data.DataLoader(
+    dataset, batch_size, shuffle=True, drop_last=True)
+    for dataset in (train_ds, train_valid_ds)]
+
+valid_iter = torch.utils.data.DataLoader(valid_ds, batch_size, shuffle=False,
+                                         drop_last=True)
+
+test_iter = torch.utils.data.DataLoader(test_ds, batch_size, shuffle=False,
+                                        drop_last=False)
+
+
+#微调预训练模型
+def get_net(devices):
+    finetune_net = nn.Sequential()
+    finetune_net.features = torchvision.models.resnet34(pretrained=True)
+    finetune_net.output_new = nn.Sequential(nn.Linear(1000, 256),
+                                            nn.ReLU(),
+                                            nn.Linear(256, 120))
+    finetune_net = finetune_net.to(devices[0])
+    for param in finetune_net.features.parameters():
+        param.requires_grad = False
+    return finetune_net
+
+#计算损失
+loss = nn.CrossEntropyLoss(reduction='none')
+
+def evaluate_loss(data_iter, net, devices):
+    l_sum, n = 0.0, 0
+    for features, labels in data_iter:
+        features, labels = features.to(devices[0]), labels.to(devices)
+        outputs = net(features)
+        l = loss(outputs, labels)
+        l_sum += l.sum()
+        n += labels.numel()
+    return l_sum / n
+
+#训练函数
+def train(net, train_iter, valid_iter, num_epochs, lr, wd, devices, lr_period,
+          lr_decay):
+    trainer = torch.optim.SGD(net.parameters(), lr=lr, momentum=0.9,
+                              weight_decay=wd)
+    scheduler = torch.optim.lr_scheduler.StepLR(trainer, lr_period, lr_decay)
+    num_batches, timer = len(train_iter), d2l.Timer()
+    legend = ['train loss', 'train acc']
+    if valid_iter is not None:
+        legend.append('valid acc')
+    animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs],
+                            legend=legend)
+    net = nn.DataParallel(net, device_ids=devices).to(devices[0])
+    for epoch in range(num_epochs):
+        net.train()
+        metric = d2l.Accumulator(3)
+        for i, (features, labels) in enumerate(train_iter):
+            timer.start()
+            l, acc = d2l.train_batch_ch13(net, features, labels,
+                                          loss, trainer, devices)
+            metric.add(l, acc, labels.shape[0])
+            timer.stop()
+            if (i + 1) % (num_batches // 5) == 0 or i == num_batches - 1:
+                animator.add(epoch + (i + 1) / num_batches,
+                             (metric[0] / metric[2], metric[1] / metric[2],
+                              None))
+        if valid_iter is not None:
+            valid_acc = d2l.evaluate_accuracy_gpu(net, valid_iter)
+            animator.add(epoch + 1, (None, None, valid_acc))
+        scheduler.step()
+    measures = (f'train loss {metric[0] / metric[2]:.3f}, '
+                f'train acc {metric[1] / metric[2]:.3f}')
+    if valid_iter is not None:
+        measures += f', valid acc {valid_acc:.3f}'
+    print(measures + f'\n{metric[2] * num_epochs / timer.sum():.1f}'
+          f' examples/sec on {str(devices)}')
+    
+
+#训练和验证模型
+devices, num_epochs, lr, wd = d2l.try_all_gpus(), 50, 1e-4, 1e-4
+lr_period, lr_decay, net = 2, 0.9, get_net(devices)
+train(net, train_iter, valid_iter, num_epochs, lr, wd, devices, lr_period,
+      lr_decay)
+
+
+#对测试集分类
+net = get_net(devices)
+train(net, train_valid_iter, None, num_epochs, lr, wd, devices, lr_period,
+      lr_decay)
+
+preds = []
+for data, label in test_iter:
+    output = torch.nn.functional.softmax(net(data.to(devices[0])), dim=1)
+    preds.extend(output.cpu().detach().numpy())
+ids = sorted(os.listdir(
+    os.path.join(data_dir, 'train_valid_test', 'test', 'unknown')))
+with open('submission.csv', 'w') as f:
+    f.write('id,' + ','.join(train_valid_ds.classes) + '\n')
+    for i, output in zip(ids, preds):
+        f.write(i.split('.')[0] + ',' + ','.join(
+            [str(num) for num in output]) + '\n')
+d2l.plt.show()
